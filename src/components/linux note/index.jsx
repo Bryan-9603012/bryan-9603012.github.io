0252105ecf6e571a48ec0f55b10bb89cfa2e0a5c
Bryan-9603012 <d14405023@ems.npu.edu.tw>
2026-02-06 16:06:47 +0800

    linux note

---
+++ src/components/linux note/index.jsx
@@ -0,0 +1,11 @@
+// src/components/linux-notes/index.js
+export { default as Permissions } from "./Permissions";
+export { default as Users } from "./Users";
+export { default as Packages } from "./Packages";
+export { default as Services } from "./Services";
+export { default as Cron } from "./Cron";
+export { default as Network } from "./Network";
+export { default as WebOps } from "./WebOps";
+export { default as Backup } from "./Backup";
+export { default as Filesystem } from "./Filesystem";
+export { default as Containers } from "./Containers";
